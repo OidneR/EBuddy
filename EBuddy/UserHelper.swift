bf02a9d2b8f36121c234e3968285a94336834517
Eldrick <81741995+OidneR@users.noreply.github.com>
2024-12-16 13:33:01 +0700

    Commit 4: Basic Upload Task

--- EBuddy/UserHelper.swift
+++ EBuddy/UserHelper.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 import FirebaseFirestore
+import FirebaseStorage
 
 final class UserHelper {
     func fetchUser() async throws -> [UserJSON]? {
@@ -16,6 +17,30 @@ final class UserHelper {
             try? Firestore.Decoder().decode(UserJSON.self, from: $0.data())
         }
     }
+    
+    func uploadProfilePicture(userID: String, image: UIImage) async -> ImageUploadStatus {
+        let storage: Storage = Storage.storage()
+        guard let imageData = image.jpegData(compressionQuality: 0.8) else {
+            return .failed
+        }
+        
+        let storageRef = storage.reference().child("profilePictures/\(userID).jpg")
+        
+        do {
+            _ = try await storageRef.putDataAsync(imageData, metadata: nil)
+            let downloadURL = try await storageRef.downloadURL()
+            return .success(downloadURL.absoluteString)
+        } catch {
+            return .error(error.localizedDescription)
+        }
+    }
+    
+}
+
+enum ImageUploadStatus {
+    case failed
+    case success(String)
+    case error(String)
 }
 
 
